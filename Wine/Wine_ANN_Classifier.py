aca7182cb065520e055ff64f517dd0ccbf67e229
Abhishek R S <abhi.apple4@gmail.com>
2017-08-30 20:09:48 +0530

    Add files via upload

--- Wine/Wine_ANN_Classifier.py
+++ Wine/Wine_ANN_Classifier.py
@@ -1,37 +1,87 @@
-# -*- coding: utf-8 -*-
-"""
-@author : Abhishek R S
-"""
+
+# coding: utf-8
+
+# ## Initialization
+
+# In[1]:
+
 
 import numpy as np
 import pandas as pd
+import keras
 
-# Import the dataset
+
+# ## Importing the Data
+
+# In[2]:
+
+
 dataset = pd.read_csv("wine.data", header = None)
 
-# Creating X and Y
+
+# In[3]:
+
+
+dataset.shape
+
+
+# In[4]:
+
+
+dataset.head()
+
+
+# ## Create X and Y
+
+# In[5]:
+
+
 X = dataset.iloc[:, 1:14].values
 Y = dataset.iloc[:, 0].values
 
-# Preprocess the Data (Label encoding
-from sklearn.preprocessing import LabelEncoder, StandardScaler, OneHotEncoder
+
+# ## Preprocess the Data
+
+# In[6]:
+
+
+from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 le_Y = LabelEncoder()
 Y = le_Y.fit_transform(Y)
 Y = Y.reshape(len(Y), 1)
 ohe = OneHotEncoder(categorical_features=[0])
 Y = ohe.fit_transform(Y).toarray()
+Y
+
+
+# In[7]:
+
+
+sc_X = StandardScaler()
+X = sc_X.fit_transform(X)
+X
+
+
+# ## Create Train and Test Data
+
+# In[8]:
+
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 4)
 
-# Scale the Data
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
 
-import keras
+# ## Create and Train the Classifier
+
+# In[9]:
+
+
 from keras.models import Sequential
 from keras.layers import Dense
+
+
+# In[10]:
+
 
 clf_ann = Sequential()
 
@@ -47,10 +97,32 @@ clf_ann.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics =
 # Train the ANN on the Training Set
 clf_ann.fit(X_train, Y_train, batch_size = 5, nb_epoch = 200)
 
-# Test the ANN on the Test Set
-Y_pred = clf_ann.predict(X_test)
-Y_pred = (Y_pred > 0.5)
 
-# Check the Accuracy
-from sklearn.metrics import accuracy_score
-accuracy_score(Y_pred, Y_test)
+# In[11]:
+
+
+Y_pred = clf_ann.predict(X_test)
+Y_pred_class = np.argmax(Y_pred, axis = 1)
+Y_test_class = np.argmax(Y_test, axis = 1)
+
+
+# ## Check the Accuracy
+
+# In[12]:
+
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+accuracy_score(Y_pred_class, Y_test_class)
+
+
+# In[13]:
+
+
+confusion_matrix(Y_pred_class, Y_test_class)
+
+
+# In[ ]:
+
+
+
+
