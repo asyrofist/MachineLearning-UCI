2e871efe394c05c8e75615f3b5370612c7e3e20f
Abhishek R S <abhi.apple4@gmail.com>
2017-08-30 19:00:39 +0530

    Add files via upload

--- BalanceScale/Balance_Scale_ANN_Classifier.py
+++ BalanceScale/Balance_Scale_ANN_Classifier.py
@@ -1,40 +1,96 @@
-# -*- coding: utf-8 -*-
-"""
-@author : Abhishek R S
-"""
 
-import numpy as np
+# coding: utf-8
+
+# ## Initialization
+
+# In[1]:
+
+
 import pandas as pd
+import numpy as np
+import keras
 
-# Import the dataset
+
+# ## Importing the Data
+
+# In[2]:
+
+
 dataset = pd.read_csv("balance-scale.data", header = None)
-   
-# Create X and Y        
+
+
+# In[3]:
+
+
 X = dataset.iloc[:, 1:5].values
 Y = dataset.iloc[:, 0].values
 
-# Preprocess the Data (Label encoding and One Hot Encoding) 
+
+# In[4]:
+
+
+dataset.head()
+
+
+# ## Preprocess the Data
+
+# In[5]:
+
+
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
 le_Y = LabelEncoder()
 Y = le_Y.fit_transform(Y)
 Y = Y.reshape(len(Y), 1)
+
+
+# In[6]:
+
+
 ohe = OneHotEncoder(categorical_features=[0])
 Y = ohe.fit_transform(Y).toarray()
 
-# Training and Test Data Split
+
+# In[7]:
+
+
+Y
+
+
+# In[8]:
+
+
+sc_X = StandardScaler()
+X = sc_X.fit_transform(X)
+
+
+# In[9]:
+
+
+X
+
+
+# ## Create Train and Test Data
+
+# In[10]:
+
+
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 4)
 
-# Scale the Data
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
 
-import keras
+# ## Create and Train the Classifier
+
+# In[11]:
+
+
 from keras.models import Sequential
 from keras.layers import Dense
 
 clf_ann = Sequential()
+
+
+# In[12]:
+
 
 # First Hidden Layer
 clf_ann.add(Dense(output_dim = 5, init = 'uniform', activation = 'relu', input_dim = 4))
@@ -48,10 +104,33 @@ clf_ann.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics =
 # Train the ANN on the Training Set
 clf_ann.fit(X_train, Y_train, batch_size = 5, nb_epoch = 200)
 
-# Test the ANN on the Test Set
-Y_pred = clf_ann.predict(X_test)
-Y_pred = (Y_pred > 0.5)
 
-# Check the Accuracy
-from sklearn.metrics import accuracy_score
-accuracy_score(Y_pred, Y_test)
+# In[13]:
+
+
+# Test the Classifier on the Test Data
+Y_pred = clf_ann.predict(X_test)
+Y_pred_class = np.argmax(Y_pred, axis = 1)
+Y_test_class = np.argmax(Y_test, axis = 1)
+
+
+# ## Check the Accuracy
+
+# In[14]:
+
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+accuracy_score(Y_pred_class, Y_test_class)
+
+
+# In[15]:
+
+
+confusion_matrix(Y_pred_class, Y_test_class)
+
+
+# In[ ]:
+
+
+
+
