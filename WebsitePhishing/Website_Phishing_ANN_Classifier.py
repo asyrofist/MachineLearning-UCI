69ba8ca6cf4b5fb98a12e797ff28897fa51b4356
Abhishek R S <abhi.apple4@gmail.com>
2017-08-30 20:23:27 +0530

    Add files via upload

--- WebsitePhishing/Website_Phishing_ANN_Classifier.py
+++ WebsitePhishing/Website_Phishing_ANN_Classifier.py
@@ -61,40 +61,40 @@ Y
 
 # ## Preprocess the Data
 
-# In[10]:
+# In[9]:
 
 
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 
 
-# In[11]:
+# In[10]:
 
 
 le_Y = LabelEncoder()
 
 
-# In[12]:
+# In[11]:
 
 
 Y = le_Y.fit_transform(Y)
 Y
 
 
-# In[13]:
+# In[12]:
 
 
 Y = Y.reshape(len(Y), 1)
 ohe_Y = OneHotEncoder(categorical_features = [0])
 
 
-# In[14]:
+# In[13]:
 
 
 Y = ohe_Y.fit_transform(Y).toarray()
 Y
 
 
-# In[15]:
+# In[14]:
 
 
 def encoder_X(index):
@@ -102,26 +102,26 @@ def encoder_X(index):
     X[:, index] = le_X.fit_transform(X[:, index])
 
 
-# In[16]:
+# In[15]:
 
 
 for x in range(0, 9):
     encoder_X(x)
 
 
-# In[17]:
+# In[16]:
 
 
 X
 
 
-# In[18]:
+# In[17]:
 
 
 ohe_X = OneHotEncoder(categorical_features = [6])
 
 
-# In[19]:
+# In[18]:
 
 
 X = ohe_X.fit_transform(X).toarray()
@@ -129,13 +129,13 @@ X = X[:, 1:]
 X.shape
 
 
-# In[20]:
+# In[19]:
 
 
 ohe_X = OneHotEncoder(categorical_features = [7])
 
 
-# In[21]:
+# In[20]:
 
 
 X = ohe_X.fit_transform(X).toarray()
@@ -143,13 +143,13 @@ X = X[:, 1:]
 X.shape
 
 
-# In[22]:
+# In[21]:
 
 
 ohe_X = OneHotEncoder(categorical_features = [8])
 
 
-# In[23]:
+# In[22]:
 
 
 X = ohe_X.fit_transform(X).toarray()
@@ -157,13 +157,13 @@ X = X[:, 1:]
 X.shape
 
 
-# In[24]:
+# In[23]:
 
 
 ohe_X = OneHotEncoder(categorical_features = [9])
 
 
-# In[25]:
+# In[24]:
 
 
 X = ohe_X.fit_transform(X).toarray()
@@ -171,13 +171,13 @@ X = X[:, 1:]
 X.shape
 
 
-# In[26]:
+# In[25]:
 
 
 ohe_X = OneHotEncoder(categorical_features = [10])
 
 
-# In[27]:
+# In[26]:
 
 
 X = ohe_X.fit_transform(X).toarray()
@@ -185,13 +185,13 @@ X = X[:, 1:]
 X.shape
 
 
-# In[28]:
+# In[27]:
 
 
 ohe_X = OneHotEncoder(categorical_features = [11])
 
 
-# In[29]:
+# In[28]:
 
 
 X = ohe_X.fit_transform(X).toarray()
@@ -199,13 +199,13 @@ X = X[:, 1:]
 X.shape
 
 
-# In[30]:
+# In[29]:
 
 
 ohe_X = OneHotEncoder(categorical_features = [12])
 
 
-# In[31]:
+# In[30]:
 
 
 X = ohe_X.fit_transform(X).toarray()
@@ -215,37 +215,37 @@ X.shape
 
 # ## Create Train and Test Data
 
-# In[32]:
+# In[31]:
 
 
 from sklearn.model_selection import train_test_split
 
 
-# In[33]:
+# In[32]:
 
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 4)
 
 
-# In[34]:
+# In[33]:
 
 
 X_train.shape
 
 
-# In[35]:
+# In[34]:
 
 
 X_test.shape
 
 
-# In[36]:
+# In[35]:
 
 
 Y_train.shape
 
 
-# In[37]:
+# In[36]:
 
 
 Y_test.shape
@@ -253,14 +253,14 @@ Y_test.shape
 
 # ## Create and train the ANN Classifier
 
-# In[38]:
+# In[37]:
 
 
 from keras.models import Sequential
 from keras.layers import Dense
 
 
-# In[51]:
+# In[38]:
 
 
 clf_ann = Sequential()
@@ -281,26 +281,33 @@ clf_ann.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics =
 clf_ann.fit(X_train, Y_train, batch_size = 10, nb_epoch = 100)
 
 
-# In[52]:
+# In[39]:
 
 
 # Test the ANN on the Test Data
 Y_pred = clf_ann.predict(X_test)
-Y_pred = (Y_pred > 0.5)
+Y_pred_class = np.argmax(Y_pred, axis = 1)
+Y_test_class = np.argmax(Y_test, axis = 1)
 
 
 # ## Check the Accuracy
 
-# In[53]:
+# In[40]:
 
 
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 
 
-# In[54]:
+# In[41]:
 
 
-accuracy_score(Y_pred, Y_test)
+accuracy_score(Y_pred_class, Y_test_class)
+
+
+# In[42]:
+
+
+confusion_matrix(Y_pred_class, Y_test_class)
 
 
 # In[ ]:
