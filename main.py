60c2f692a800e19de45b12461770ad28b4b1c184
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-16 16:18:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ Berikut ini algoritma yang digunakan untuk Dataset UCI
 """)
 
 st.header("UCI Dataset")
-dataset = pd.read_csv("diagnosis.data", header = None, delimiter = r"\s+", encoding = "utf-16")
+dataset = pd.read_csv("AcuteInflammations/diagnosis.data", header = None, delimiter = r"\s+", encoding = "utf-16")
 hasil = dataset.head()
 st.write(hasil)
