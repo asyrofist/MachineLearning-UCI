8b8514145b7a620c1a4638401c171f61f4b0a811
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-16 14:55:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import streamlit as st
 import numpy as np
 import pandas as pd
 
 # dataset
+st.write("""
+# Machine Learning
+Berikut ini algoritma yang digunakan untuk Dataset UCI
+""")
+st.header("UCI Dataset")
 dataset = pd.read_csv("diagnosis.data", header = None, delimiter = r"\s+")
 X = dataset.iloc[:, 0:6].values
 Y1 = dataset.iloc[:, 6:7].values
