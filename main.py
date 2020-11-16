695ad23bd43e41ea54f2c710ad9745799ce57d92
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-16 16:59:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,11 +17,9 @@ st.write(hasil)
 start_X, end_X = st.sidebar.select_slider('Select Range?',options=[0, 6, 8],value=(0, 6))
 X = dataset.iloc[:, start_X:end_X].values
 st.write(X)
-start_Y1, end_Y1 = st.sidebar.select_slider('Select Range?',options=[0, 6, 8],value=(6, 7))
-Y1 = dataset.iloc[:, start_Y1:end_Y1].values
+Y1 = dataset.iloc[:, start_X:end_X+1].values
 st.write(Y1)
-start_Y2, end_Y2 = st.sidebar.select_slider('Select Range?',options=[0, 6, 8],value=(7, 8))
-Y2 = dataset.iloc[:, start_Y2:end_Y2].value
+Y2 = dataset.iloc[:, start_X+2:end_X+3].value
 st.write(Y2)
 
 Y1 = Y1.reshape(len(Y1), 1)
