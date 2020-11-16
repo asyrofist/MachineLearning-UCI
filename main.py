e3744d431190a6d49e94255c41addf822daeaa37
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-16 16:06:05 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import csv
-
 
 # dataset
 st.write("""
@@ -11,5 +9,20 @@ Berikut ini algoritma yang digunakan untuk Dataset UCI
 """)
 
 st.header("UCI Dataset")
-dataset = pd.read_csv("AcuteInflammations/diagnosis.data", header = None, delimiter = r"\s+", encoding = 'utf-8')
-st.dataframe(dataset)
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+     # To read file as bytes:
+     bytes_data = uploaded_file.read()
+     st.write(bytes_data)
+
+     # To convert to a string based IO:
+     stringio = StringIO(uploaded_file.decode("utf-8"))
+     st.write(stringio)
+
+     # To read file as string:
+     string_data = stringio.read()
+     st.write(string_data)
+
+     # Can be used wherever a "file-like" object is accepted:
+     dataframe = pd.read_csv(uploaded_file)
+     st.write(dataframe)
