1e2447f830d49cf0cb67df2d2d19933b478bea19
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-16 18:31:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ Y2 = Y2.reshape(len(Y2), 1)
 
 # Create distplot with custom bin_size
 st.subheader("X Parameter")
-pic0 = ff.create_distplot(X, [1,2,3,4])
+pic0 = ff.create_distplot(X, [0,1,2,3,4])
 st.plotly_chart(pic0, use_container_width=True)
 
 st.subheader("Y1 Parameter")
