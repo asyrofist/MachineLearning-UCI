797579951cf24d8712ac6c65ab54716b8d4cc932
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-16 15:30:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,8 @@ Berikut ini algoritma yang digunakan untuk Dataset UCI
 """)
 
 st.header("UCI Dataset")
-with open("AcuteInflammations/diagnosis.data", encoding="utf8", errors='ignore') as dataset:
-  st.dataframe(dataset)
+with open("AcuteInflammations/diagnosis.data",'rb') as f:
+    dataset = f.read()
+dataset = dataset.rstrip("\n").decode("utf-16")
+dataset = dataset.split("\r\n")
+st.dataframe(dataset)
