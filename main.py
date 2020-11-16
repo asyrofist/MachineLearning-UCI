e7a62abc732a93229dff356d07e0411f151e9e6c
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-16 16:43:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,15 @@ option = st.sidebar.selectbox('How would you like to be contacted?',['diagnosis'
 dataset = pd.read_csv('data/'+option+'.data', header = None, delimiter = r"\s+", encoding = "utf-16")
 hasil = dataset.head()
 st.write(hasil)
+
+X = dataset.iloc[:, 0:6].values
+Y1 = dataset.iloc[:, 6:7].values
+Y2 = dataset.iloc[:, 7:8].values
+st.write(X)
+st.write(Y1)
+st.write(Y2)
+
+Y1 = Y1.reshape(len(Y1), 1)
+Y2 = Y2.reshape(len(Y2), 1)
+st.write(Y1)
+st.write(Y2)
