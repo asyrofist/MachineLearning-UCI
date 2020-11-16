2bd55f789e385b767f30e1107077321808d5c648
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-16 15:16:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ st.write("""
 # Machine Learning
 Berikut ini algoritma yang digunakan untuk Dataset UCI
 """)
+
 st.header("UCI Dataset")
 dataset = pd.read_csv("AcuteInflammations/diagnosis.data", header = None, delimiter = r"\s+")
 X = dataset.iloc[:, 0:6].values
 Y1 = dataset.iloc[:, 6:7].values
 Y2 = dataset.iloc[:, 7:8].values
 a = dataset.head()
-st.write(a)
+st.dataframe(a)
