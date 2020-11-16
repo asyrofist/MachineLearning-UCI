fdb2ddfab2771c04851be0f254674f964569181f
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-16 16:40:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ Berikut ini algoritma yang digunakan untuk Dataset UCI
 """)
 
 st.header("UCI Dataset")
-option = st.sidebar.selectbox('How would you like to be contacted?',['Diagnosis', 'yeast', 'wine'))
+option = st.sidebar.selectbox('How would you like to be contacted?',['Diagnosis', 'yeast', 'wine'])
 dataset = pd.read_csv(option, header = None, delimiter = r"\s+", encoding = "utf-16")
 hasil = dataset.head()
 st.write(hasil)
