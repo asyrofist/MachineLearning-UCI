e5823904feac6598a5ea8cfb521bd9d56e1e6314
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-16 16:09:22 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,19 +11,7 @@ Berikut ini algoritma yang digunakan untuk Dataset UCI
 st.header("UCI Dataset")
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
-     
      # To read file as bytes:
      bytes_data = uploaded_file.read()
-     st.dataframe(bytes_data)
-
-     # To convert to a string based IO:
-     stringio = StringIO(uploaded_file.decode("utf-8"))
-     st.dataframe(stringio)
-
-     # To read file as string:
-     string_data = stringio.read()
-     st.dataframe(string_data)
-
-     # Can be used wherever a "file-like" object is accepted:
-     dataframe = pd.read_csv(uploaded_file)
+     dataframe = pd.read_csv(bytes_data)
      st.dataframe(dataframe)
