cab742607ecc7d40ce156eec82452b9b60c562c9
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-16 15:50:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,5 @@ Berikut ini algoritma yang digunakan untuk Dataset UCI
 
 st.header("UCI Dataset")
 with open("AcuteInflammations/diagnosis.data", 'rb') as csvfile:
-     spamreader = pd.read_csv(csvfile, delimiter= r"\s+")
-#      for row in spamreader:
-#          print ', '.join(row)
+     spamreader = pd.read_csv(csvfile, header = None, delimiter= r"\s+")
 st.dataframe(spamreader)
