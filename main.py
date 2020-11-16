7714d7e6eec32ed76083bcc606261e8fdc5f4889
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-16 18:56:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -80,16 +80,18 @@ st.write(X)
 st.header("Create and Train the Classifier for Y1")
 X1_train, X1_test, Y1_train, Y1_test = train_test_split(X, Y1, test_size = 0.2, random_state = 4)
 X2_train, X2_test, Y2_train, Y2_test = train_test_split(X, Y2, test_size = 0.2, random_state = 4)
+st.write(X1_train, X1_test, Y1_train, Y1_test)
+st.write(X2_train, X2_test, Y2_train, Y2_test)
 
-# model
-clf_ann = Sequential()
-clf_ann.add(Dense(3, activation = 'relu', kernel_initializer='glorot_uniform', input_dim = 6)) # First Hidden Layer
-clf_ann.add(Dense(1, activation = 'sigmoid', kernel_initializer='glorot_uniform')) # Output Layer
-clf_ann.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy']) # Compile the ANN
-clf_ann.fit(X1_train, Y1_train, batch_size = 5, epochs = 200) # Train the ANN on the Training Set
+# # model
+# clf_ann = Sequential()
+# clf_ann.add(Dense(3, activation = 'relu', kernel_initializer='glorot_uniform', input_dim = 6)) # First Hidden Layer
+# clf_ann.add(Dense(1, activation = 'sigmoid', kernel_initializer='glorot_uniform')) # Output Layer
+# clf_ann.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy']) # Compile the ANN
+# clf_ann.fit(X1_train, Y1_train, batch_size = 5, epochs = 200) # Train the ANN on the Training Set
 
-#prediction
-Y1_pred = clf_ann.predict(X1_test) # Test the ANN on the Test Data
-Y1_pred = (Y1_pred > 0.5)
-akurasi = accuracy_score(Y1_test, Y1_pred)
-st.write(akurasi)
+# #prediction
+# Y1_pred = clf_ann.predict(X1_test) # Test the ANN on the Test Data
+# Y1_pred = (Y1_pred > 0.5)
+# akurasi = accuracy_score(Y1_test, Y1_pred)
+# st.write(akurasi)
