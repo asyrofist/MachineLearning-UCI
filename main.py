7ad68273488794cfce9195883d39db6cc25b4d40
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-16 17:28:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -15,14 +15,13 @@ hasil = dataset.head()
 st.write(hasil)
 
 # variable
-start_X, end_X = st.select_slider('Select X range?',options=[0, 1, 2, 3, 4, 5, 6, 7, 8],value=(0, 6))
-start_Y1, end_Y1 = st.select_slider('Select Y1 range?',options=[0, 1, 2, 3, 4, 5, 6, 7, 8],value=(end_X, end_X+1))
-start_Y2, end_Y2 = st.select_slider('Select Y2 range?',options=[0, 1, 2, 3, 4, 5, 6, 7, 8],value=(end_Y1, end_Y1+1))
+start_X, end_X = st.sidebar.select_slider('Select X range?',options=[0, 1, 2, 3, 4, 5, 6, 7, 8],value=(0, 6))
+start_Y1, end_Y1 = st.sidebar.select_slider('Select Y1 range?',options=[0, 1, 2, 3, 4, 5, 6, 7, 8],value=(end_X, end_X+1))
+start_Y2, end_Y2 = st.sidebar.select_slider('Select Y2 range?',options=[0, 1, 2, 3, 4, 5, 6, 7, 8],value=(end_Y1, end_Y1+1))
 X = dataset.iloc[:, start_X:end_X].values
 Y1 = dataset.iloc[:, start_Y1:end_Y1].values
 Y2 = dataset.iloc[:, start_Y2:end_Y2].values
-nilai_list = st.multiselect('What are your favorite colors',[X, Y1, Y2],[X])
-st.dataframe(nilai_list)
+st.dataframe(X, Y1, Y2)
 
 Y1 = Y1.reshape(len(Y1), 1)
 Y2 = Y2.reshape(len(Y2), 1)
