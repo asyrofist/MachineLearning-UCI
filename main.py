d5b64661521c217a47218c8e2963c430d53e6ced
Rakha Asyrofi <rakhasyrofist@gmail.com>
2020-11-16 22:07:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,94 +1,64 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder
+import seaborn as sns
+
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, confusion_matrix
-import keras
-from keras.models import Sequential
-from keras.layers import Dense
 
 
-# dataset
-st.write("""
-# Machine Learning
-Berikut ini algoritma yang digunakan untuk Dataset UCI
-""")
+def main():
 
-st.header("Importing Data")
-file_data = st.sidebar.selectbox('How would you like to be contacted?',['diagnosis', 'yeast', 'wine'])
-dataset = pd.read_csv('data/'+file_data+'.data', header = None, delimiter = r"\s+", encoding = "utf-16")
-hasil = dataset.head()
-st.write(hasil)
+    df = load_data()
 
-# variable
-start_X, end_X = st.sidebar.select_slider('Select X range?',options=[0, 1, 2, 3, 4, 5, 6, 7, 8],value=(0, 6))
-start_Y1, end_Y1 = st.sidebar.select_slider('Select Y1 range?',options=[0, 1, 2, 3, 4, 5, 6, 7, 8],value=(end_X, end_X+1))
-start_Y2, end_Y2 = st.sidebar.select_slider('Select Y2 range?',options=[0, 1, 2, 3, 4, 5, 6, 7, 8],value=(end_Y1, end_Y1+1))
-X = dataset.iloc[:, start_X:end_X].values
-Y1 = dataset.iloc[:, start_Y1:end_Y1].values
-Y2 = dataset.iloc[:, start_Y2:end_Y2].values
-Y1 = Y1.reshape(len(Y1), 1)
-Y2 = Y2.reshape(len(Y2), 1)
+    page = st.sidebar.selectbox("Choose a page", ['Homepage', 'Exploration', 'Prediction'])
+
+    if page == 'Homepage':
+        st.title('Wine Alcohol Class Prediction')
+        st.text('Select a page in the sidebar')
+        st.dataframe(df)
+    elif page == 'Exploration':
+        st.title('Explore the Wine Data-set')
+        if st.checkbox('Show column descriptions'):
+            st.dataframe(df.describe())
+        
+        st.markdown('### Analysing column relations')
+        st.text('Correlations:')
+        fig, ax = plt.subplots(figsize=(10,10))
+        sns.heatmap(df.corr(), annot=True, ax=ax)
+        st.pyplot()
+        st.text('Effect of the different classes')
+        sns.pairplot(df, vars=['magnesium', 'flavanoids', 'nonflavanoid_phenols', 'proline'], hue='alcohol')
+        st.pyplot()
+    else:
+        st.title('Modelling')
+        model, accuracy = train_model(df)
+        st.write('Accuracy: ' + str(accuracy))
+        st.markdown('### Make prediction')
+        st.dataframe(df)
+        row_number = st.number_input('Select row', min_value=0, max_value=len(df)-1, value=0)
+        st.markdown('#### Predicted')
+        st.text(model.predict(df.drop(['alcohol'], axis=1).loc[row_number].values.reshape(1, -1))[0])
 
 
-#X Parameter
-st.write(X)
+@st.cache
+def train_model(df):
+    X = np.array(df.drop(['alcohol'], axis=1))
+    y= np.array(df['alcohol'])
 
-#Y1 Parameter
-col3, col4 = st.beta_columns([3,1])
-pic1, ax = plt.subplots()
-ax.hist(Y1, bins=20)
-col3.pyplot(pic1)
-col4.write(Y1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# X Parameter
-col5, col6 = st.beta_columns([3,1])
-pic2, ax = plt.subplots()
-ax.hist(Y2, bins=20)
-col5.pyplot(pic2)
-col6.write(Y2)
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
 
-st.header("Preprocess the Data")
-le_Y = LabelEncoder()
-Y1 = le_Y.fit_transform(Y1)
-Y2 = le_Y.transform(Y2)
+    return model, model.score(X_test, y_test)
 
-col6, col7 = st.beta_columns([3,1])
-pic3, ax = plt.subplots()
-ax.hist(Y1, bins=20)
-col6.pyplot(pic3)
-col7.write(Y1)
+@st.cache
+def load_data():
+    return pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', names=['alcohol', 'malic_acid', 'ash', 'alcalinity_of_ash', 'magnesium', 'total_phenols','flavanoids', 'nonflavanoid_phenols' ,'proanthocyanins', 'color_intensity', 'hue', 'OD280/OD315_of_diluted_wines', 'proline'], delimiter=",", index_col=False)
 
-col8, col9 = st.beta_columns([3,1])
-pic4, ax = plt.subplots()
-ax.hist(Y2, bins=20)
-col8.pyplot(pic4)
-col9.write(Y2)
 
-le_X = LabelEncoder()
-le_X.fit(X[:, 1])
-
-X[:, 1] = le_X.transform(X[:, 1])
-X[:, 2] = le_X.transform(X[:, 2])
-X[:, 3] = le_X.transform(X[:, 3])
-X[:, 4] = le_X.transform(X[:, 4])
-X[:, 5] = le_X.transform(X[:, 5])
-st.write(X)
-
-st.header("Create and Train the Classifier for Y1")
-X1_train, X1_test, Y1_train, Y1_test = train_test_split(X, Y1, test_size = 0.2, random_state = 4)
-
-# model
-clf_ann = Sequential()
-clf_ann.add(Dense(3, activation = 'relu', kernel_initializer='glorot_uniform', input_dim = 6)) # First Hidden Layer
-clf_ann.add(Dense(1, activation = 'sigmoid', kernel_initializer='glorot_uniform')) # Output Layer
-clf_ann.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy']) # Compile the ANN
-clf_ann.fit((X1_train).astype(np.float32), (Y1_train).astype(np.float32), batch_size = 5, epochs = 200) # Train the ANN on the Training Set
-
-# #prediction
-# Y1_pred = clf_ann.predict(X1_test) # Test the ANN on the Test Data
-# Y1_pred = (Y1_pred > 0.5)
-# akurasi = accuracy_score(Y1_test, Y1_pred)
-# st.write(akurasi)
+if __name__ == '__main__':
+    main()
